1290286630505ea2eac27db5190d492f9e949200
Ensar <ensaraliu2017@gmail.com>
2026-03-30 22:38:11 +0200

    1- auth controllers added

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/auth.routes.js";
-import { connect } from "mongoose";
 import connectMongoDB from "./db/connectMongoDB.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  }),
+);
+
+app.use(express.json()); // Middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
+app.use(cookieParser()); // Middleware to parse cookies
 
 app.use("/api/auth", authRoutes);
 
