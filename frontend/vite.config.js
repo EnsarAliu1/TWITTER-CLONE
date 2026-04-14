465fbf5ebe46074b046353381fe6a2f793e48417
Ensar <ensaraliu2017@gmail.com>
2026-04-14 16:43:22 +0200

    3- fetch posts for the home page

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,8 +8,10 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "/api": "http://localhost:5000",
-      changeOrigin: true,
+      "/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+      },
     },
   },
 });
